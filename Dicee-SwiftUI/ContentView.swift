c4e9c9de78adf23e5b8d42a2d6997ff6f1cd47ec
Yedige Ashirbek <edige11020@gmail.com>
2022-12-15 09:41:19 +0600

    Added function to button

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Yedige Ashirbek on 15.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(diceNumber: leftDiceNumber)
+                    DiceView(diceNumber: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                .padding(.all)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiceView: View {
+
+    let diceNumber: Int
+    
+    var body: some View {
+        Image("dice\(diceNumber)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.all)
+    }
+}
